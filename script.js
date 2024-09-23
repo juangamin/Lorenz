0387a32a45874858ca60deb11441eb4dbfb648d5
juangamin <53021780+juangamin@users.noreply.github.com>
2024-09-23 15:31:28 -0600

    Update script.js add

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.153.0/build/three.module.js';
+import GUI from 'https://cdn.jsdelivr.net/npm/lil-gui@0.17.1/dist/lil-gui.esm.js';
+import { TrackballControls } from 'https://cdn.jsdelivr.net/npm/three@0.153.0/examples/jsm/controls/TrackballControls.js';
+
 
 // Set up the scene, camera, and renderer
 const scene = new THREE.Scene();
