85848abbc47a2e8a670eb406e656e53365262989
juangamin <53021780+juangamin@users.noreply.github.com>
2024-09-24 13:27:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ controls.dynamicDampingFactor = 0.15;
 const axesHelper = new THREE.AxesHelper(5);
 const points = [];
 const points2 = [];
-const axespoints = [];
+let axespoints;
 
   var geometry = new THREE.BufferGeometry().setFromPoints( points );
   var geometry2 = new THREE.BufferGeometry().setFromPoints( points2 );
@@ -65,7 +65,30 @@ const axespoints = [];
   var curve2 = new THREE.Line( geometry2, material2 );
 
   for ( let i = -30; i < 32; i+=2 ){
+        for ( let i = -30; i < 32; i+=2 ){
       axespoints = [];
+      axespoints.push( new THREE.Vector3( -30,i,0) );
+      axespoints.push( new THREE.Vector3( 30,i,0) );
+      const geometry3 = new THREE.BufferGeometry().setFromPoints( axespoints );
+      const material3 = new THREE.LineBasicMaterial({color: 0xFFFFFF});
+      const line3 = new THREE.Line( geometry3, material3 );
+      scene.add(line3);
+      axespoints = [];
+      axespoints.push( new THREE.Vector3( i,-30,0) );
+      axespoints.push( new THREE.Vector3( i,30,0) );
+      const geometry4 = new THREE.BufferGeometry().setFromPoints( axespoints );
+      const material4 = new THREE.LineBasicMaterial({color: 0xFFFFFF});
+      const line4 = new THREE.Line( geometry4, material4 );
+      scene.add( line4 );
+    }
+    axespoints = [];
+    axespoints.push( new THREE.Vector3( 0,0,0) );
+    axespoints.push( new THREE.Vector3( 0,0,60) );
+    const geometry5 = new THREE.BufferGeometry().setFromPoints( axespoints );
+    const material5 = new THREE.LineBasicMaterial({color: 0xFFFFFF});
+    const line5 = new THREE.Line( geometry5, material5 );
+    scene.add( line5 );
+
       axespoints.push( new THREE.Vector3( -30,i,0) );
       axespoints.push( new THREE.Vector3( 30,i,0) );
       const geometry3 = new THREE.BufferGeometry().setFromPoints( axespoints );
